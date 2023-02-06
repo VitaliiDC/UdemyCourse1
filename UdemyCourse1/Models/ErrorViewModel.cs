304e553875f9b86776345c066707772ce3c40606
Vitalii Drobit <cdrobitvitalii@gmail.com>
2023-02-06 18:31:51 -0500

    Add project files.

---
+++ UdemyCourse1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UdemyCourse1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
